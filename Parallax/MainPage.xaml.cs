c30f7ad53c680b3507a96e3a95c1dd0cd4365942
PABLO ERICK CARDOSO <pablo@cloudinfo.pt>
2019-09-16 16:47:58 +0100

    ini

---
+++ Parallax/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Parallax
+{
+
+    public partial class MainPage : ContentPage
+    {
+        private double depth = 200; // FORCE OF PARALLAX
+        private List<MainViewModel> mainViewModel;
+        private int ajustDevice = Device.RuntimePlatform == Device.Android ? 2 : 1;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            mainViewModel = new List<MainViewModel>() {
+                 new MainViewModel{ Photo="pic2.png", Title = "PARALLAX", BackgroundColor= Color.Lime},
+                 new MainViewModel{ Photo="pic5.png", Title = "EASY", BackgroundColor= Color.Red},
+                 new MainViewModel{ Photo="pic3.png", Title = "JUST COPY", BackgroundColor= Color.Pink},
+                 new MainViewModel{ Photo="pic6.png", Title = "NO PLUGINS", BackgroundColor= Color.Orange},
+                 new MainViewModel{ Photo="pic7.png", Title = "FORMS 4.2", BackgroundColor= Color.Blue},
+                 new MainViewModel{ Photo="pic10.png", Title = "FAST, NO BUGS", BackgroundColor= Color.Silver},
+            };
+            this.BindingContext = mainViewModel;
+            pgCarousel.ItemsSource = mainViewModel;
+        }
+
+
+
+        public void Handle_Scrolled(object sender, ItemsViewScrolledEventArgs e)
+        {
+            var _currentIndex = e.CenterItemIndex;
+            var widthCarousel = pgCarousel.Width;
+            var posOffset = (widthCarousel * (_currentIndex + 1)) - (e.HorizontalOffset / ajustDevice);
+            var pos = (((posOffset * depth) / widthCarousel) - depth);
+
+            mainViewModel[e.LastVisibleItemIndex].Position = pos + depth;
+            mainViewModel[_currentIndex].Position = pos;
+            
+        }
+
+       
+
+
+    }
+}
---
+++ Parallax/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace Parallax
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public string Title { get; set; }
+        public Color BackgroundColor { get; set; }
+        public string Photo { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private double _position;
+        public double Position
+        {
+            get { return _position; }
+            set
+            {
+                if (_position != value)
+                {
+                    _position = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
